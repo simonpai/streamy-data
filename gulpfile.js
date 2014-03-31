8a14be42bec026436529dea1d34df4c26bd84a2b
simonpai <simonpai@potix.com>
2014-03-31 12:06:01 +0800

    Fine tune.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp'),
 	mocha = require('gulp-mocha');
 
-var array = require('stream-array'),
-	sdata = require('./index');
-
 gulp.task('default', ['build', 'test']);
 
 gulp.task('build', function() {
@@ -14,13 +11,5 @@ gulp.task('test', function() {
 	
 	return gulp.src(['test/*.js'])
 		.pipe(mocha({ reporter: 'dot' }));
-	
-});
-
-gulp.task('demo', function() {
-	
-	return sdata.ptt.board('Gossiping', { limit: 10 })
-		.pipe(sdata.ptt.article())
-		.pipe(sdata.echo());
 	
 });
