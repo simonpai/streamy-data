fc044cf86417e08df010ad3e92272c5038bd6e8f
simonpai <simonpai@potix.com>
2014-04-28 16:29:37 +0800

    Revive tests.

---
+++ test/array.js
@@ -0,0 +1,30 @@
+var assert = require('assert'),
+	gulp = require('gulp'),
+	streamy = require('../index.js');
+
+describe('array', function () {
+	
+	this.timeout(3000);
+	
+	it('array A', function (done) {
+			
+		var len = 50,
+			input = [],
+			output = [];
+		
+		for (var i = 0; i < len; i++)
+			input.push(i);
+		
+		streamy.array(input)
+			.on('data', output.push.bind(output))
+			.on('end', function () {
+				assert.equal(output.length, len, "Output length should be " + len);
+				for (var i = 0; i < len; i++)
+					assert.equal(output[i], i, 
+						"Unmatched content: actual: " + output[i] + " / expected: " + i);
+				done();
+			});
+		
+	});
+	
+});
